39875f7fec14454568dc4a349fed78a8531c622b
= Md. Kawser Ahmed <kawserahmed8492@gmail.com>
2023-03-22 07:51:44 +0600

    addshow donw

--- crud_operation_with_base_class_view/urls.py
+++ crud_operation_with_base_class_view/urls.py
@@ -19,7 +19,7 @@ from enroll import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.add_show,name="addandshow"),
+    path("",views.AddShow.as_view(),name="addandshow"),
     path("delete/<int:id>/",views.delete_data,name="deletedata"),
     path("update/<int:id>/",views.update_data,name="updatedata"),
 ]
--- enroll/views.py
+++ enroll/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render,HttpResponseRedirect
 from .forms import *
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
-# This function will add student and show the student 
-def add_show(request):
-    if request.method == "POST":
+# This function will add student and show the student
+class AddShow(TemplateView):
+    template_name = 'enroll/addandshow.html'
+    def get_context_data(self,*args,**kwargs):
+        context = super().get_context_data(**kwargs)
+        fm = StudentRegistration()
+        stud = User.objects.all()
+        context = {"form":fm,"stu":stud}
+        return context
+    def post(self,request):
         fm = StudentRegistration(request.POST)
         if fm.is_valid():
             fm.save()
             fm = StudentRegistration()
-    else:
-        fm = StudentRegistration()
-    Stud = User.objects.all()
-    return render(request,"enroll/addandshow.html",{"form":fm,"stu":Stud})
+            return HttpResponseRedirect('/')
+
+
+
+# def add_show(request):
+#     if request.method == "POST":
+#         fm = StudentRegistration(request.POST)
+#         if fm.is_valid():
+#             fm.save()
+#             fm = StudentRegistration()
+#     else:
+#         fm = StudentRegistration()
+#     Stud = User.objects.all()
+#     return render(request,"enroll/addandshow.html",{"form":fm,"stu":Stud})
 
 # This function is responsible for updating data
 def update_data(request,id):
